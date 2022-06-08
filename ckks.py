19d138571e2c8ca9a244249fb4a9ee981d5e4022
Jeremy Stevens <jsteve22@nd.edu>
2022-06-08 12:57:21 -0400

    Create new ckks.py file

---
+++ ckks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Date: 6/8/2022
+# Create a Class to emulate ckks encryption scheme 
+# 
+# This program will create a class for the ckks 
+# fully homomorphic encryption scheme
+
+class ckks():
+
+  def __init__():
+    return
+
+  
+
+def main():
+
+  return
+
+if __name__ == '__main__':
+  main()
