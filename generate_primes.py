d232bde9dcadee38d56a64cade62fff350bf9474
Jeremy Stevens <jsteve22@nd.edu>
2022-07-25 17:59:20 -0400

    Change prime generation

--- generate_primes.py
+++ generate_primes.py
@@ -17,9 +17,81 @@ def main():
 		primes = line.split(',')
 		primes = [int(p) for p in primes]
 
-		with open('prime.pickle','wb') as prim_file:
+		with open('100000primes.pickle','wb') as prim_file:
 				pkl.dump( primes, prim_file )
 
+def SieveOfEratosthenes(num):
+	# this function will generate all primes less
+	# than given num
+
+	try: 
+		prime = [True]*(num+1)
+	except:
+		prime = [True for i in range(num+1)]
+
+	p = 2
+	while ( p*p <= num ):
+
+		if prime[p] == True:
+			for i in range(p*p, num+1, p):
+				prime[i] = False
+		p += 1
+	
+	prime.pop(0)
+	prime.pop(0)
+
+	primes = []
+	for ind, p in enumerate(prime):
+		if p:
+			primes.append(ind+2)
+	
+	# print( primes )
+	print( primes[-5:] )
+	print( len(primes) )
+	return primes
+
+def Sieve(num):
+	# find all primes up to and including num
+
+	primes = [-1]*(2**32)
+	primes[0] = 2
+	sz = 1
+	p = 3
+
+	while ( p < num ):
+		is_prime = True
+		for prim in primes[:sz]:
+			if prim*prim > p:
+				break
+			if p % prim == 0:
+				is_prime = False
+
+		if is_prime:
+			primes.append(p)
+			sz += 1
+
+		p += 1
+
+	return primes
+
+def testSieve():
+	p = SieveOfEratosthenes(1299827)
+
+	with open('100000primes.pickle','rb') as f:
+		primes = pkl.load(f)
+		print(f'Sieve == prime.pickle: {p == primes}')
+
+def generatePickle():
+	# this will save generated primes to pickle file
+	# primes = Sieve(2**20)
+	primes = SieveOfEratosthenes(2**20)
+	return
+	with open('primes.pickle','wb') as f:
+		pkl.dump( primes, f )
 
 if __name__ == '__main__':
-	main()
+	# main()
+	# SieveOfEratosthenes(2**23)
+	# testSieve()
+	generatePickle()
+
